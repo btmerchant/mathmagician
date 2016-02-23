7f8e686abd21ceb1e6907dd9356486b3e819d278
Brian Merchant <btmerchant@comcast.net>
2016-02-22 21:32:04 -0600

    Initial commit

---
+++ Mathmagician.Tests/IntegerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class IntegerTests
+    {
+        [TestMethod]
+        public void IntergerEnsureICanCreateAnInstance()
+        {
+            IntegerTests my_int = new IntegerTests();
+            Assert.IsNotNull(my_int);
+        }
+        //use testm tab tab
+        [TestMethod]
+        public void IntegerInsureICanGetFirstNumber()
+        {
+            // Arange -Scenario Setup
+            IntegerTests my_int = new IntegerTests();
+            // Act - Do the thing you want to test
+            int actual = my_int.GetFirst();
+            int expected = 0;
+            // Assert did it work as expected
+            Assert.AreEquals(expected, actual);
+        }
+        [TestMethod]
+        public void IntegerEnsureICanGetNextInteger()
+        {
+            // Arange -Scenario Setup
+            IntegerTests my_int = new IntegerTests();
+            // Act - Do the thing you want to test
+            int actual = my_int.GetNext(5);
+            int expected = 6;
+            // Assert did it work as expected
+            Assert.AreEquals(expected, actual);
+        }
+        [TestMethod]
+        public void IntegerEsureICanCreateASequenceOfTenIntegers()
+        {
+            //Look at MSDN CollectionAssert class ie AreEqual
+            // Arange -Scenario Setup
+            IntegerTests my_int = new IntegerTests();
+            // Act - Do the thing you want to test
+            int[] actual = my_int.GetSequence();
+            int[] expected = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // Assert did it work as expected
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof)]
+        public void IntegerInsureMaxIsNotExceded()
+        {
+            // Arange -Scenario Setup
+            IntegerTests my_int = new IntegerTests();
+            // Act - Do the thing you want to test
+            int[] actual = my_int.GetSequence(some_int);
+            Assert.Throws(typeof(Exception))
+        }
+    }
+}
--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -8,6 +8,39 @@ namespace Mathmagician
 {
     class Program
     {
+        public class Integer
+        {
+            
+            public Integer GetFirst()
+            {
+                return 0;
+            }
+            public Integer GetNext()
+            {
+                return current + 1;
+            }
+            public int[] GetSequence(int how_many)
+            {
+                if (how_many > MAX)
+                {
+                    
+                }
+                int[] seq_array = new int[how_many];
+                int counter = 0;
+                seq_array[counter] = GetFirst();
+                while (counter < how_many - 1)
+                {
+                    //seq_array[counter + 1] = GetNext(seq_array[counter]);
+                    counter++;
+                    seq_array[counter + 1] = GetNext(seq_array[counter - 1]);
+                }
+                //for (int i = 0, i < how_many,   i++)
+                //{
+                //    seq_array[i];
+                //}
+                return seq_array;
+            }
+        }
         static void Main(string[] args)
         {
         }
